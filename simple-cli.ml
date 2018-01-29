274cb2e44910f0ab7696482faa9c149ac69ba51c
Theo Kalfas <kalfastheodoros@gmail.com>
2018-01-28 20:20:28 -0600

    More sane behavior with command line flags

--- simple-cli.ml
+++ simple-cli.ml
@@ -2,23 +2,46 @@
 open Arg
 open Printf
 
-let printArg arg =
-  print_endline arg
+type mode =
+  | Print
+  | Length
+  | Help
 
-let printLength arg =
-  printf "%d\n" (String.length arg)
+let cliMode = ref Print
+
+(* A reverse list of the arguments, along with *)
+(*    an add operation and a print operation   *)
+let argList = ref []
+
+let addArg arg =
+  argList := arg :: !argList
+
+let printArgs () =
+  List.iter print_endline (List.rev !argList)
+
+let printLengths () =
+  List.iter (printf "%d\n") (List.map String.length (List.rev !argList))
 
 (* Usage message *)
 let printHelp () =
   print_endline "Usage: simple-cli [flags] [args]";
   print_endline "Available flags:";
   print_endline "  -length     prints the lengths of each of the arguments";
-  print_endline "  -help       prints this help message";
-  exit 0; ()
+  print_endline "  -help       prints this help message";;
 
 (* Speclist for Arg Module *)
-let speclist = [("-length", Arg.Rest printLength, "prints the lengths of each of the arguments");
-                ("-help", Arg.Unit printHelp, "prints this help message")]
+let speclist = [
+  ("-length", Arg.Unit (fun () -> cliMode := Length),
+      "prints the lengths of each argument");
+  ("-help",   Arg.Unit (fun () -> cliMode := Help), "prints this help message")
+]
+
+let main () =
+  Arg.parse speclist addArg "Usage?";
+  match !cliMode with
+  | Print  -> printArgs ()
+  | Help   -> printHelp ()
+  | Length -> printLengths ()
 
 (* Main call to Arg.parse *)
-let () = Arg.parse speclist printArg "Usage?"
+let () = main ()
