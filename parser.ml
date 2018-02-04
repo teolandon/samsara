be9cd951e35d1929be59f33160495ccde51ebb3b
Theo Kalfas <kalfastheodoros@gmail.com>
2018-02-04 04:38:11 -0600

    Added error message for programs with trailing chars

--- parser.ml
+++ parser.ml
@@ -39,21 +39,37 @@ let rec computeAST_h listRef =
     | (t::ts) -> listRef := ts; t
   in
   match readToken () with
-  | EInt x -> ELit x
+  | EInt x -> ((ELit x), !listRef)
   | ELeftParen ->
       (match readToken () with
       | EPlus ->
-          let tree1 = computeAST_h listRef in
-          let tree2 = computeAST_h listRef in
+          let (tree1, _) = computeAST_h listRef in
+          let (tree2, _) = computeAST_h listRef in
           let sum = ESum (tree1, tree2) in
           (match readToken () with
-          | ERightParen -> sum
+          | ERightParen -> (sum, !listRef)
           | _           ->
               raise (Invalid_token "Addition expresion not closed")
           )
       | _ -> raise (Invalid_token "No operator found after paren")
       )
   | _ -> raise (Invalid_token "Expression not valid")
+
+let rec evaluateAST tree =
+  match tree with
+  | ELit a              -> a
+  | ESum (tree1, tree2) -> (evaluateAST tree1) + (evaluateAST tree2)
+
+let computeAST tokenList =
+  let listRef = ref tokenList in
+  let (tree, remainingTokens) = computeAST_h listRef in
+  match remainingTokens with
+  | [] -> tree
+  | _  -> raise (Invalid_token "Program not valid: trailing characters")
+
+let createAndEvaluate tokenList =
+  let tree = computeAST tokenList in
+  evaluateAST tree
 
 let rec printAST_h tree depth =
   let indent = String.make depth ' ' in
@@ -64,16 +80,6 @@ let rec printAST_h tree depth =
       printAST_h tree1 (depth+1);
       printAST_h tree2 (depth+1);
       printf "%s:fo muS\n" indent
-
-let rec evaluateAST tree =
-  match tree with
-  | ELit a              -> a
-  | ESum (tree1, tree2) -> (evaluateAST tree1) + (evaluateAST tree2)
-
-let createAndEvaluate tokenList =
-  let listRef = ref tokenList in
-  let tree = computeAST_h listRef in
-  evaluateAST tree
 
 let printAST tree =
   printAST_h tree 0
