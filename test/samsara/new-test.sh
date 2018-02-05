e655136837def8baab8766b19f89198542cb02fd
Theo Kalfas <kalfastheodoros@gmail.com>
2018-02-05 16:40:10 -0600

    Added test files, and a script to easily generate more tests

---
+++ test/samsara/new-test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+NUMS=$(seq -s, -w 0 100)
+
+IFS=', ' read -r -a array <<< "$NUMS"
+
+NUM=$(ls -1 "$DIR/arith" | wc -l)
+
+NAME=$1
+TEXT=$2
+RESULT=$3
+
+if [[ "$NAME" != *.arith ]]; then
+  NAME="$NAME.arith"
+fi
+
+echo "$TEXT" > "$DIR/arith/${array[NUM]}-${NAME}"
+
+printf '%s:\n\t%s' "$NAME" "$RESULT" >> "$DIR/correct.out"
+
